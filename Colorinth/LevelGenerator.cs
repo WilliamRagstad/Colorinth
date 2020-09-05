ed4068ca7c468a18c9fee2f4a5f74d50afb35b3f
Mikael Skog <mikael_skog@outlook.com>
2020-09-05 12:39:52 +0200

    Added LevelGenerator.cs and VS Code support

---
+++ Colorinth/LevelGenerator.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Colorinth
+{
+    public class LevelGenerator
+    {
+        public struct Level {
+            int sizeX, sizeY;
+            List<char> levelArray;
+        }
+
+        //public bool GenerateLevel(int numOfColors, int sizeX, int sizeY, int buttonTileRatio, int gateWallRatio) {
+
+        //}
+    }
+}
