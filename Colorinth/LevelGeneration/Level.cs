294c9d2823b2b6fd6c2d796b60028fe7d4ba6c0f
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-09-06 14:18:17 +0200

    Refactor Level

--- Colorinth/LevelGeneration/Level.cs
+++ Colorinth/LevelGeneration/Level.cs
@@ -4,63 +4,26 @@ namespace Colorinth.LevelGeneration
 {
     public class Level
     {
-        private byte sizeX, sizeY;
-        private List<char> tileList;
-        private List<char> horizontalEdgeList;
-        private List<char> verticalEdgeList;
+        public byte SizeX, SizeY;
+        private readonly List<char> _tileList = new List<char>();
+        private readonly List<char> _horizontalEdgeList = new List<char>();
+        private readonly List<char> _verticalEdgeList = new List<char>();
 
         public Level(byte sizeX, byte sizeY)
         {
-            this.sizeX = sizeX;
-            this.sizeY = sizeY;
+            SizeX = sizeX;
+            SizeY = sizeY;
 
-            tileList = new List<char>();
-            horizontalEdgeList = new List<char>();
-            verticalEdgeList = new List<char>();
+            for (int i = 0; i < sizeX*sizeY; i++) _tileList.Add('.');
+            for (int i = 0; i < sizeX*(sizeY-1); i++) _horizontalEdgeList.Add('.');
+            for (int i = 0; i < (sizeX-1)*sizeY; i++) _verticalEdgeList.Add('.');
+        }
 
-            for (int i = 0; i < sizeX*sizeY; i++) tileList.Add('.');
-            for (int i = 0; i < sizeX*(sizeY-1); i++) horizontalEdgeList.Add('.');
-            for (int i = 0; i < (sizeX-1)*sizeY; i++) verticalEdgeList.Add('.');
-        }
-        public void SetSizeX(byte size)
-        {
-            sizeX = size;
-        }
-        public byte GetSizeX()
-        {
-            return sizeX;
-        }
-        public void SetSizeY(byte size)
-        {
-            sizeY = size;
-        }
-        public byte GetSizeY()
-        {
-            return sizeY;
-        }
-        public void SetTileAt(int index, char tile)
-        {
-            tileList[index] = tile;
-        }
-        public char GetTileAt(int index)
-        {
-            return tileList[index];
-        }
-        public void SetHorizontalEdgeAt(int index, char edge)
-        {
-            horizontalEdgeList[index] = edge;
-        }
-        public char GetHorizontalEdgeAt(int index)
-        {
-            return horizontalEdgeList[index];
-        }
-        public void SetVerticalEdgeAt(int index, char edge)
-        {
-            verticalEdgeList[index] = edge;
-        }
-        public char GetVerticalEdgeAt(int index)
-        {
-            return verticalEdgeList[index];
-        }
+        public void SetTileAt(int index, char tile) => _tileList[index] = tile;
+        public char GetTileAt(int index) => _tileList[index];
+        public void SetHorizontalEdgeAt(int index, char edge) => _horizontalEdgeList[index] = edge;
+        public char GetHorizontalEdgeAt(int index) => _horizontalEdgeList[index];
+        public void SetVerticalEdgeAt(int index, char edge) => _verticalEdgeList[index] = edge;
+        public char GetVerticalEdgeAt(int index) => _verticalEdgeList[index];
     }
 }
