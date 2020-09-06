a2bcbca9f3b82d6ba13b48a194b20f1655ebee82
William Rågstad <41281398+WilliamRagstad@users.noreply.github.com>
2020-09-06 16:35:47 +0200

    Added functionality and sprite loading

--- Colorinth/Extensions/LevelDrawer.cs
+++ Colorinth/Extensions/LevelDrawer.cs
@@ -10,7 +10,16 @@ namespace Colorinth.Extensions
 {
     public static class LevelDrawer
     {
+        #region Properties
+
+        private static readonly int _spriteWidth = 195;
+
+        #endregion
+        #region Textures
+        
         private static Texture2D _startTexture, _finishTexture;
+
+        #endregion
         public static void Initialize(ContentManager content)
         {
             _startTexture = content.Load<Texture2D>("start");
@@ -19,7 +28,29 @@ namespace Colorinth.Extensions
 
         public static void DrawLevel(this SpriteBatch spriteBatch, GraphicsDevice graphicsDevice, Level level, Rectangle area)
         {
+            int tileOffset = area.Width / level.SizeX / 2;
+            float spriteScale = 1;
 
+            for (int y = 0; y < level.SizeY; y++)
+            {
+                for (int x = 0; x < level.SizeX; x++)
+                {
+                    char tile = level.GetTileAt(y * level.SizeY + x);
+                    
+                    int cx = area.X + x - _spriteWidth / 2 + tileOffset;
+                    int cy = area.Y + y - _spriteWidth / 2 + tileOffset;
+
+                    switch (tile)
+                    {
+                        case 'S':
+                            spriteBatch.Draw(_startTexture, new Vector2(cx, cy), null, Color.White, 0, Vector2.Zero, spriteScale, SpriteEffects.None, 0);
+                            break;
+                        case 'F':
+                            spriteBatch.Draw(_finishTexture, new Vector2(cx, cy), null, Color.White, 0, Vector2.Zero, spriteScale, SpriteEffects.None, 0);
+                            break;
+                    }
+                }
+            }
         }
     }
 }
